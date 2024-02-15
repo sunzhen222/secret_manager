d792f111dd32d627ce1028d41c28153e4460727f
Leon <sunzhen_222@163.com>
2024-02-15 22:10:28 +0800

    secret encrypt/decrypt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 //use serde_json::Result;
 use serde_json;
 use serde_json::json;
-use serde_json::Value;
+//use serde_json::Value;
 use anyhow::Result;
 use std::env;
 use std::fs;
@@ -17,79 +17,23 @@ struct SecretInfo {
     secret: String,
 }
 
-struct PersonalInfo {
-    secret_info: Vec<SecretInfo>,
-}
-
 fn main() -> Result<()> {
 
     let args: Vec<String> = env::args().collect();
 
-    let filename = &args[2];
+    let filename = args[2].to_string();
     let query = &args[1];
 
     println!("query:{}", query);
     println!("filename:{}", filename);
     println!("In file {}", filename);
 
-    if Path::new(filename).is_file() == false {
-        println!("{} not exist", filename);
-        File::create(filename)?;
-    }
-    let file_content = fs::read(filename)?;
-    if file_content.len() == 0 {
-        println!("empty file");
-    } else {
-        secret::get_plain(file_content, "test password".to_string());
-    }
-    let secret_info1 = SecretInfo {
+    let secret_info1: SecretInfo = SecretInfo {
         name: "google".to_string(),
         account: "jy70342".to_string(),
         secret: "8262".to_string(),
     };
-    let secret_info2 = SecretInfo {
-        name: "google".to_string(),
-        account: "jy70342".to_string(),
-        secret: "8262".to_string(),
-    };
-
-    //let secret_array = [secret_info1, secret_info2];
-//
-    //let json_string = serde_json::to_string(&secret_info1)?;
-    //println!("Serialized JSON: {}", json_string);
-
-    //let personal_info = PersonalInfo {
-    //    secret_info: vec![secret_info1, secret_info2],
-    //};
-    //let personal_info = PersonalInfo {
-    //    secret_info: vec![
-    //        SecretInfo {
-    //            name: "John".to_string(),
-    //            account: "john@example.com".to_string(),
-    //            secret: "password123".to_string(),
-    //        },
-    //        SecretInfo {
-    //            name: "Alice".to_string(),
-    //            account: "alice@example.com".to_string(),
-    //            secret: "securepassword".to_string(),
-    //        },
-    //    ],
-    //};
-    let personal_info = json!({
-        "secret_info": vec![secret_info1, secret_info2],
-    });
-
-
-    let json_string = serde_json::to_string(&personal_info)?;
-    println!("json_string JSON: {}", json_string);
-
-    let v: Value = serde_json::from_str(&json_string)?;
-    let secret_info = v["secret_info"].to_string();
-    //println!("v: {}", secret_info);
-    let secret_info: Vec<SecretInfo> = serde_json::from_str(&secret_info)?;
-    println!("name: {}", secret_info[0].name);
-
-    //edit_secret(&secret_info1, "123".to_string());
+    edit_secret(secret_info1, filename, "test password".to_string())?;
 
     Ok(())
 }
@@ -97,11 +41,29 @@ fn main() -> Result<()> {
 
 
 fn edit_secret(
-    secret_info: &SecretInfo,
+    secret_info: SecretInfo,
+    filename: String,
     password: String,
-) {
-    println!("name={}", secret_info.name);
-    println!("account={}", secret_info.account);
-    println!("secret={}", secret_info.secret);
-    println!("password={}", password);
+) -> Result<()> {
+    if Path::new(&filename).is_file() == false {
+        println!("{} not exist", filename);
+        File::create(&filename)?;
+    }
+    let file_content = fs::read(&filename)?;
+    let mut secret_info_vec: Vec<SecretInfo> = Vec::new();
+    if file_content.len() == 0 {
+        println!("empty file");
+        secret_info_vec.push(secret_info);
+        let personal_info = json!({
+            "secret_info": secret_info_vec,
+        });
+        let json_string = serde_json::to_string(&personal_info)?;
+        println!("json_string JSON: {}", json_string);
+        let encrypt_data = secret::encrypt(json_string, password);
+        fs::write(filename, encrypt_data)?;
+    } else {
+        let plain_string = secret::decrypt(file_content, "test password".to_string());
+        println!("plain_string={}", plain_string);
+    }
+    Ok(())
 }
--- src/secret.rs
+++ src/secret.rs
@@ -1,13 +1,14 @@
 use sha2::{Sha512, Digest};
-use std::fmt::Write;
+//use std::fmt::Write;
+//use std::ptr::null;
 use aes::Aes256;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
-use hex_literal::hex;
+//use hex_literal::hex;
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 
-pub fn get_plain(cipher: Vec<u8>, password: String) -> String {
+pub fn encrypt(plain: String, password: String) -> Vec<u8> {
     let mut hash = Sha512::new();
     hash.update(password.as_bytes());
     let result = hash.finalize();
@@ -15,48 +16,36 @@ pub fn get_plain(cipher: Vec<u8>, password: String) -> String {
     let key: Vec<u8> = first_half.to_vec();
     let iv: Vec<u8> = second_half[0..16].to_vec();
 
-    //let mut ori_data = Vec::new();
-    //let mut index = 0;
-    //loop {
-    //    ori_data.push(index);
-    //    index += 1;
-    //    if index >= 32 {
-    //        break;
-    //    }
-    //}
-    //let ori_data = "hello".as_bytes();
-    let string: String = key.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    println!("key={}", string);
-    let string: String = iv.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    println!("iv={}", string);
+    let cipher = encrypt_aes_cbc(plain.as_bytes(), &key, &iv);
+    //let string: String = cipher.iter().map(|&byte| format!("{:02X}", byte)).collect();
+    //println!("cipher={}", string);
+
+    cipher
+}
+
+pub fn decrypt(cipher: Vec<u8>, password: String) -> String {
+    let mut hash = Sha512::new();
+    hash.update(password.as_bytes());
+    let result = hash.finalize();
+    let (first_half, second_half) = result.split_at(32);
+    let key: Vec<u8> = first_half.to_vec();
+    let iv: Vec<u8> = second_half[0..16].to_vec();
+
+    //let string: String = key.iter().map(|&byte| format!("{:02X}", byte)).collect();
+    //println!("key={}", string);
+    //let string: String = iv.iter().map(|&byte| format!("{:02X}", byte)).collect();
+    //println!("iv={}", string);
 
     //let cipher = encrypt_aes_cbc(&ori_data, &key, &iv);
-    let string: String = cipher.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    println!("cipher={}", string);
+    //let string: String = cipher.iter().map(|&byte| format!("{:02X}", byte)).collect();
+    //println!("cipher={}", string);
     let plain = decrypt_aes_cbc(&cipher, &key, &iv);
-    let string: String = plain.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    println!("plain={}", string);
 
-    //let cipher_str: String = cipher.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    //println!("cipher={}", cipher_str);
     //let plain = decrypt_aes_cbc(&cipher, &key, &iv);
     //let plain: String = plain.iter().map(|&byte| format!("{:02X}", byte)).collect();
     //println!("plain={}", plain);
 
-    //let first_vec: String = first_vec.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    //println!("first_vec={}", first_vec);
-    //let second_vec: String = second_vec.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    //println!("second_vec={}", second_vec);
-
-    //let mut hex_string = String::new();
-    //write!(&mut hex_string, "{:x}", result).expect("Failed to write hex string");
-    //println!("result={}", result.len());
-    //println!("hex_string={}", hex_string);
-
-    //let cipher: String = cipher.iter().map(|&byte| format!("{:02X}", byte)).collect();
-    //println!("cipher={}", cipher);
-
-    "123".to_string()
+    String::from_utf8(plain).unwrap()
 }
 
 
